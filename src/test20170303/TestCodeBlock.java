effedb13b9bbdda6161224148f3b84c1d3136b3b
yunzhifei <yunzhifei@58ganji.com>
2017-03-03 17:06:43 +0800

    Rejected commit: Default

--- src/test20170303/TestCodeBlock.java
+++ src/test20170303/TestCodeBlock.java
@@ -2,7 +2,34 @@ package test20170303;
 
 /**
  * Created by 58 on 2017/3/3.
+ * author yun zhi fei
  */
 public class TestCodeBlock {
 
+
+    public static void main(String[] args) {
+        TestCoder.testStatic();
+        TestCoder testCoder = new TestCoder();
+        System.out.println("testCoder = " + testCoder.hashCode());
+
+    }
+}
+
+class TestCoder {
+
+    static void testStatic() {
+        System.out.println("静态方法！");
+    }
+
+    static {
+        System.out.println("静态代码块！");
+    }
+
+    {
+        System.out.println("代码块！");
+    }
+
+    TestCoder() {
+        System.out.println("构造");
+    }
 }
